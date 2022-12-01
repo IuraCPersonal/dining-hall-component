c825c5a888b52fd51b287ea2a06680f0d368fb96
IuraCPersonal <iurie.cius.personal@gmail.com>
2022-12-01 17:07:34 +0200

    feat(*): 70% implementation for lab 2

--- app/Table.py
+++ app/Table.py
@@ -1,8 +1,9 @@
-import queue, random, time
+import queue, time, random
+
 from threading import Thread
 
 from app.modules import *
-from app.helpers.Print import Print
+from app.utils.Print import Print
 
 
 # Extend the Thread class to create Threads for the Tables.
@@ -12,17 +13,46 @@ class Table(Thread):
         self.table_id = table_id
         self.__private_list = queue.Queue()
     
-    def __get_max_wait_time(self, items):
-        wait_time = [FOOD[food_id]['preparation-time'] for food_id in items]
-        return max(wait_time) * 1.3
 
-    def __get_priority(self, items):
-        # METHOD 1:
-        # priority = len(items)
+    # Overide the run() method of the Thread class.
+    def run(self):
+        while True:
+            # Table FREE. Waiting for people to come...
+            time.sleep(random.randint(5, 30) * TIME_UNIT)
 
-        # METHOD 2:
-        priority = sum(FOOD[item]["preparation-time"] for item in items)
-        return priority
+            # Table READY to order.
+            order_queue.put(self.table_id)
+            
+            # NOTE! the call to get() will block until an item is available to retrieve from the queue.
+            order_ready = self.__private_list.get()
+
+            # Table SERVED. Eating...
+            order_stars = self.__review_order(order_ready)
+        
+            Print.serve_order(order_ready["waiter_id"], order_ready["order_id"], order_ready["table_id"], order_stars)
+
+
+    def generate_order(self, waiter_id):
+        order_id = order_indexer.get_index()
+        menu_items = restaurants.get(str(RESTAURANT_ID)).get('menu_items')
+        items = [random.randint(1, menu_items) for _ in range(random.randint(1, 10))]
+
+        order = {
+            'order_id': order_id,
+            'table_id': self.table_id,
+            'waiter_id': waiter_id,
+            'items': items,
+            'priority': None,
+            'max_wait': None,
+            'pick_up_time': time.time()
+        }
+
+        return order
+
+
+    def serve_order(self, content):
+        self.__private_list.put(content)
+
 
     def __review_order(self, order):
         stars = 0
@@ -40,41 +70,3 @@ class Table(Thread):
             stars = 5
         
         return stars
-    
-    # Overide the run() method of the Thread class.
-    def run(self):
-        while True:
-            # Table FREE. Waiting for people to come...
-            time.sleep(random.randint(5, 30) * TIME_UNIT)
-
-            # Table READY to order.
-            order_queue.put(self.table_id)
-
-            # NOTE! the call to get() will block until an item is available to retrieve from the queue.
-            order_ready = self.__private_list.get()
-            # Table SERVED. Eating...
-            order_stars = self.__review_order(order_ready)
-
-            Print.serve_order(order_ready["waiter_id"], order_ready["order_id"], order_ready["table_id"], order_stars)
-
-            reputation_system.append(order_stars)
-            Print.reputation(reputation_system)
-
-    def generate_order(self, waiter_id):
-        order_id = order_indexer.get_index()
-        items = [random.randint(1, 12) for _ in range(random.randint(1, 10))]
-
-        order = {
-            'order_id': order_id,
-            'table_id': self.table_id,
-            'waiter_id': waiter_id,
-            'items': items,
-            'priority': self.__get_priority(items),
-            'max_wait': self.__get_max_wait_time(items),
-            'pick_up_time': time.time()
-        }
-
-        return order
-
-    def serve_order(self, content):
-        self.__private_list.put(content)
--- app/Waiter.py
+++ app/Waiter.py
@@ -2,8 +2,9 @@ import queue
 import requests
 
 from app.modules import *
-from threading import Thread
-from app.helpers.Print import Print
+from threading import Thread, Lock
+from app.utils.Print import Print
+
 
 # Extend the Thread class to create Threads for the Waiters.
 class Waiter(Thread):
@@ -11,7 +12,10 @@ class Waiter(Thread):
         super(Waiter, self).__init__(name=f'Waiter-{waiter_id}', *args, **kwargs)
         self.waiter_id = waiter_id
         self.__private_list = queue.Queue()
+        self.__lock = Lock()
 
+
+    # Overide the run() method of the Thread class.
     def run(self):
         while True:
             try:
@@ -19,21 +23,67 @@ class Waiter(Thread):
                 threads[f'Table-{finished_order["table_id"]}'].serve_order(finished_order)
             except queue.Empty:
                 pass
+            except KeyError:
+                pass
 
             try:
                 # This line will raise queue.Empty if the queue is actually empty
                 free_table_id = order_queue.get(block=False)
+
                 order = threads[f'Table-{free_table_id}'].generate_order(self.waiter_id)
+                items = order.get('items')
+                
+                order['priority'] = self.__get_priority(items)
+                order['max_wait'] = self.__get_max_wait_time(items)
 
                 Print.took_order(self.waiter_id, order["order_id"], order["table_id"])
 
                 _ = requests.post(
-                    url=f'http://kitchen:{KITCHEN_PORT}/order',
+                    url=f'http://kitchen-{RESTAURANT_ID}:{KITCHEN_PORT}/order',
                     json=order
                 )
+            except queue.Empty:
+                pass
+        
+            
+            # Check if there are any online orders waiting to be indexed and sent to kitchen.
+            try:
+                online_order = online_order_queue.get(block=False)
+                items = online_order.get('items')
 
+                online_order.update({
+                    'order_id': order_indexer.get_index(),
+                    'is_ready': False,
+                    'priority': self.__get_priority(items),
+                    'max_wait': self.__get_max_wait_time(items),
+                    'waiter_id': self.waiter_id
+                })
+
+                _ = requests.post(
+                    url=f'http://kitchen-{RESTAURANT_ID}:{KITCHEN_PORT}/order',
+                    json=online_order
+                )
             except queue.Empty:
                 pass
 
+
+    def __get_max_wait_time(self, items):
+        wait_time = [
+            restaurants.get(str(RESTAURANT_ID)).get('menu').get(str(item)).get('preparation-time') 
+            for item in items
+        ]
+
+        return max(wait_time) * 1.3
+
+
+    def __get_priority(self, items):
+        priority = sum(
+            restaurants.get(str(RESTAURANT_ID)).get('menu').get(str(item)).get('preparation-time') 
+            for item in items
+        )
+
+        return priority
+
     def serve_order(self, content):
-        self.__private_list.put(content)
+        with self.__lock:
+            self.__private_list.put(content)
